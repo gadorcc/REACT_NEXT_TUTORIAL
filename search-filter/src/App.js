d9c729bf7946cb72f624597f43dc79db0780340b
Gador Cruz <gadorcruz@hotmail.com>
2023-06-22 15:45:38 +0100

    search bar

--- search-filter/src/App.js
+++ search-filter/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+export default function Searchfilter() {
+  const [filterName, setFilterName] = useState('')
+  let filterHandler = (e) = {
+    var lowercase = e.target.value.toLowerCase();
+    setFilterName(e.target.value.toLowerCase());
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+  <>
+  <SearchName
+  filterName = {filterName}
+  onFilterTextChange = {setFilterName} />
+  {/* <TableName = onFilterTextChange = {setFilterName}> */}
+  <TableName/>
+  </>
+
+);
 }
 
-export default App;
+function SearchName ({filterName, onFilterTextChange}) {
+  return (
+  <form>
+    <input type="text" value={filterName} placeholder="Search Name" onChange={(e) => onFilterTextChange(e.target.value)} />
+  </form>
+  )
+}
+
+function TableName() {
+  const NAMES =
+[{
+    "id": 1,
+    "text": "Devpulse"
+}, {
+    "id": 2,
+    "text": "Linklinks"
+}, {
+    "id": 3,
+    "text": "Centizu"
+}, {
+    "id": 4,
+    "text": "Dynabox"
+}, {
+    "id": 5,
+    "text": "Avaveo"
+}, {
+    "id": 6,
+    "text": "Demivee"
+}, {
+    "id": 7,
+    "text": "Jayo"
+}, {
+    "id": 8,
+    "text": "Blognation"
+}, {
+    "id": 9,
+    "text": "Podcat"
+}, {
+    "id": 10,
+    "text": "Layo"
+}]
+  return (
+    <ul>
+      {NAMES.map((name) => (
+        <li key={name.id}>{name.text}</li>
+      ))}
+    </ul>
+  )
+}
